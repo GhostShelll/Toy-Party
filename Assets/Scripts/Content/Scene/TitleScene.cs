f152e35425bb3fa7f96973fbc268c87acebd641a
JBG <gohi2001@nate.com>
2022-06-24 21:45:39 +0900

    - Title Scene 기본 틀 구현

--- Assets/Scripts/Content/Scene/TitleScene.cs
+++ Assets/Scripts/Content/Scene/TitleScene.cs
@@ -1,9 +1,47 @@
+using com.jbg.content.popup;
 using com.jbg.core.scene;
 
 namespace com.jbg.content.scene
 {
     public class TitleScene : SceneEx
     {
-        // TODO[jbg] : 채우자
+        // TODO[jbg] : 필요한 것만 사용해야함
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+
+            SystemPopupAssist.OpenNoticeOneBtnPopup("JBG TITLE", "JBG MESSAGE\nJBG MESSAGE\nJBG MESSAGE\n", null);
+        }
+
+        protected override void OnClose()
+        {
+            base.OnClose();
+        }
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        protected override void OnBack()
+        {
+            base.OnBack();
+        }
+
+        protected override void OnAppSuspend()
+        {
+            base.OnAppSuspend();
+        }
+
+        protected override void OnAppResume()
+        {
+            base.OnAppResume();
+        }
+
+        protected override void Refresh()
+        {
+            base.Refresh();
+        }
     }
 }
