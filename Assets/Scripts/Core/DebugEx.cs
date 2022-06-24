18d6e214dcd2ae50f09260213f91b5f36902f480
JBG <gohi2001@nate.com>
2022-06-24 21:45:06 +0900

    - Log Tag 수정

--- Assets/Scripts/Core/DebugEx.cs
+++ Assets/Scripts/Core/DebugEx.cs
@@ -6,7 +6,7 @@ namespace com.jbg.core
 {
     public class DebugEx
     {
-        private static readonly string tag = "TODO[jbg]";
+        private static readonly string tag = "[JBG]";
 
         [Conditional("LOG_DEBUG")]
         public static void Log(object msg)
