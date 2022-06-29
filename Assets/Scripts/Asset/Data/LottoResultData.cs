369c39fa8e7ad7787be62a4e08e9789942d66697
JBG <gohi2001@nate.com>
2022-06-29 17:51:17 +0900

    - Lotto Data - Lotto Table Asset에 대한 Class 추가

---
+++ Assets/Scripts/Asset/Data/LottoResultData.cs
@@ -0,0 +1,15 @@
+namespace com.jbg.asset.data
+{
+    public class LottoResultData
+    {
+        public int code;
+
+        public int num1;
+        public int num2;
+        public int num3;
+        public int num4;
+        public int num5;
+        public int num6;
+        public int bonus;
+    }
+}
