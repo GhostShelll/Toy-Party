889082e5f93b6d3991ec2ed8a9564ed2fdf02fa1
GhostShell <gohi2001@nate.com>
2022-09-13 18:21:30 +0900

    Block System : Block 관련 경로 정보 변경

--- Assets/Scripts/Content/Block/BlockManager.cs
+++ Assets/Scripts/Content/Block/BlockManager.cs
@@ -8,7 +8,8 @@ namespace com.jbg.content.block
 {
     public class BlockManager : ComponentEx
     {
-        private const string BLOCK_BASE_PATH = "Sprites/Blocks/";
+        private const string BLOCK_PREFAB_PATH = "Prefabs/Blocks/";
+        private const string BLOCK_IMAGE_PATH = "Sprites/Blocks/";
 
         public enum Color
         {
@@ -45,7 +46,7 @@ namespace com.jbg.content.block
 
         private BlockCell[][] blockMap;
 
-        public void OnOpen()
+        public void Initialize()
         {
             Transform cached = this.CachedTransform;
 
@@ -70,32 +71,32 @@ namespace com.jbg.content.block
             //Transform t;
 
             this.normalBlockSprites = new();
-            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_b"));
-            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_g"));
-            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_o"));
-            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_p"));
-            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_r"));
-            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_y"));
+            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_b"));
+            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_g"));
+            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_o"));
+            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_p"));
+            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_r"));
+            this.normalBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_y"));
 
             this.packBlockSprites = new();
-            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_b_pack"));
-            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_g_pack"));
-            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_o_pack"));
-            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_p_pack"));
-            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_r_pack"));
-            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_y_pack"));
+            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_b_pack"));
+            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_g_pack"));
+            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_o_pack"));
+            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_p_pack"));
+            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_r_pack"));
+            this.packBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_y_pack"));
 
             this.blockLineSprites = new();
-            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_b_vertical_line"));
-            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_g_vertical_line"));
-            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_o_vertical_line"));
-            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_p_vertical_line"));
-            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_r_vertical_line"));
-            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_y_vertical_line"));
+            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_b_vertical_line"));
+            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_g_vertical_line"));
+            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_o_vertical_line"));
+            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_p_vertical_line"));
+            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_r_vertical_line"));
+            this.blockLineSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_y_vertical_line"));
 
             this.specialBlockSprites = new();
-            this.specialBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_z_munchkin"));
-            this.specialBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_BASE_PATH + "blk_z_rainbow"));
+            this.specialBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_z_munchkin"));
+            this.specialBlockSprites.Add(Resources.Load<Sprite>(BlockManager.BLOCK_IMAGE_PATH + "blk_z_rainbow"));
         }
 #endif  // UNITY_EDITOR
     }
